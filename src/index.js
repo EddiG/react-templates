c12db7543ea0b99e41108fcd1951e5c1cea43b6c
Eduard Gilmutdinov <edgilmutdinov@gmail.com>
2017-05-12 11:59:10 +0500

    Change the path of requiring the main App container

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
-import App from './containers/App';
+import App from 'containers/App';
 
 const render = Component => {
   ReactDOM.render(
@@ -16,7 +16,7 @@ render(App);
 
 // Hot Module Replacement API
 if (module.hot) {
-  module.hot.accept('./containers/App', () => {
+  module.hot.accept('containers/App', () => {
     render(App);
   });
 }
